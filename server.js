d01af7f6aa4b7e17f95eec3bccbecf0eae95002f
Ivan <havanaguilahava@gmail.com>
2020-03-28 22:52:19 -0300

    Proxying for server.js

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const httpProxy = require('http-proxy');
+
+const app = express();
+
+app.use(express.static(__dirname + '/dist/test-tecso-frontend'));
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/dist/test-tecso-frontend/index.html'));
+});
+app.listen(80, function () {
+  console.log("Express server listening on port 80");
+});
+var proxy = httpProxy.createProxyServer({});
+app.all("*/api/*", function (req, res) {
+  console.log("old request url " + req.url)
+  //req.url = '/' + req.url.split('/').slice(2).join('/'); // remove the '/api' part
+  console.log("new request url " + req.url)
+  proxy.web(req, res, {
+    host: "https://sb-tecso-examen.herokuapp.com/api",
+    port: 80
+  });
+});
